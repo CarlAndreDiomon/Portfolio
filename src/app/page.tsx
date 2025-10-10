152a9c8c18b8a15474f21e4fc6414b67add1fff5
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-10-10 10:07:15 +0800

    Fix projects section and experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
             <h1 className="font-bold md:text-4xl ">Carl Andre R. Diomon</h1>
             <h2 className=" text-xs md:text-base ">Antique, Philippines</h2>
             <h3 className="text-gray-500 text-xs md:text-base ">Future Software Engineer</h3>
+            {/* <h3 className="text-gray-500 text-xs md:text-base ">dcarlandrerafols@gmail.com</h3> */}
+
           </div>
         </div>
 
@@ -45,34 +47,45 @@ export default function Home() {
           <div className="grid gap-4 ">
 
             <div className="w-full h-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg">
+              {/* App name */}
               <h1 className="font-bold md:text-lg">
-                Project 1
+                I-Learn
               </h1>
+
+              {/* Description */}
               <p className="text-sm md:text-base">
-                A simple web application offers a wide opportunity for testing and debugging purpose only.
-                technology use is latest and perfect hahaha
+                An educational mobile application designed to support both offline and online learning.
+                Teachers can upload modules, and students can download and store them locally. Features are
+                Role-based authentication (students, teachers, admins), secure file handling,
+                and periodic login validation for access control. I handle backend development.
+                <br></br>
+                <br></br>
               </p>
+
+              {/* Technologies used */}
+              <div className=" flex flex-wrap gap-2 mb-1">
+                <h1 className="text-xs border w-max p-1 rounded-sm md:text-sm">
+                  MongoDB
+                </h1>
+
+                <h1 className="text-xs border w-max p-1 rounded-sm md:text-sm">
+                  Express.js
+                </h1>
+
+                <h1 className="text-xs border w-max p-1 rounded-sm md:text-sm">
+                  React
+                </h1>
+
+                <h1 className="text-xs border w-max p-1 rounded-sm md:text-sm">
+                  Node.js
+                </h1>
+
+                <h1 className="text-xs border w-max p-1 rounded-sm md:text-sm">
+                  Next.js
+                </h1>
+              </div>
             </div>
 
-            <div className="w-full h-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg">
-              <h1 className="font-bold md:text-lg">
-                Project 1
-              </h1>
-              <p className="text-sm md:text-base">
-                A simple web application offers a wide opportunity for testing and debugging purpose only.
-                technology use is latest and perfect hahaha
-              </p>
-            </div>
-
-            <div className="w-full h-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg">
-              <h1 className="font-bold md:text-lg">
-                Project 1
-              </h1>
-              <p className="text-sm md:text-base">
-                A simple web application offers a wide opportunity for testing and debugging purpose only.
-                technology use is latest and perfect hahaha
-              </p>
-            </div>
           </div>
         </div>
 
@@ -224,20 +237,13 @@ export default function Home() {
 
           <div className="grid gap-4">
             {/* Each row */}
-            <div className="grid grid-cols-[1fr_auto] items-start">
-              <div>
-                <h1 className="font-semibold md:text-lg">Software Developer</h1>
-                <h2 className="text-xs md:text-base">Iloilo City</h2>
-              </div>
-              <span className="ml-2 p-1 border rounded-lg text-xs font-normal md:text-base">2028</span>
-            </div>
 
             <div className="grid grid-cols-[1fr_auto] items-start">
               <div>
                 <h1 className="font-semibold md:text-lg">BS Information Technology</h1>
                 <h2 className="text-xs md:text-base">University of Antique</h2>
               </div>
-              <span className="ml-2 p-1 border rounded-lg text-xs font-normal md:text-base">2027</span>
+              <span className="ml-2 p-1 border rounded-lg text-xs font-normal md:text-base">2025</span>
             </div>
 
             <div className="grid grid-cols-[1fr_auto] items-start">
@@ -250,6 +256,10 @@ export default function Home() {
           </div>
         </div>
 
+        {/* Contact info */}
+        <div>
+
+        </div>
 
         <footer className="flex justify-center items-center  bottom-0 w-full h-12 ">
           <h1>&copy; 2025 SoftEn. All Rights Reserved</h1>
